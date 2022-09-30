b82f8accede030a085d2586bcc5032ca1e92c728
Drei4 <antipin-lukinf@mail.ru>
2022-09-30 14:04:16 +0300

    add zadach9 (dodelat)

--- dz2/zadach9.py
+++ dz2/zadach9.py
@@ -1,2 +1,24 @@
 #4) Задайте число N и создайте список, заполненный числами из промежутка [-N, N].
 # Найдите произведение элементов на указанных позициях. Позиции хранятся в файле file.txt в одной строке одно число.
+
+n = int(input('Введите целое число : '))
+order = []
+#i = -n
+
+for i in range(-n, n+1):
+    order.append(i)
+
+print(order)
+
+chis = []
+path = 'C:\\Users\\antip\\Desktop\\учеба\\python\\DZ\\dz2\\chisla.txt'
+data = open(path, 'r')
+for line in data:
+    chis.append(line)
+print(chis)
+
+# ch = chis
+# list(map(int, ch))
+# print(chis)
+
+
