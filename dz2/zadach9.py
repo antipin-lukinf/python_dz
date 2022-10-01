13c51e1ab6c3295e2028fae36c054d2c9116d270
Drei4 <antipin-lukinf@mail.ru>
2022-10-01 13:12:20 +0300

    add zadach9 dodelal

--- dz2/zadach9.py
+++ dz2/zadach9.py
@@ -3,12 +3,13 @@
 
 n = int(input('Введите целое число : '))
 order = []
-#i = -n
+
 
 for i in range(-n, n+1):
     order.append(i)
 
 print(order)
+
 
 chis = []
 path = 'C:\\Users\\antip\\Desktop\\учеба\\python\\DZ\\dz2\\chisla.txt'
@@ -17,8 +18,26 @@ for line in data:
     chis.append(line)
 print(chis)
 
-# ch = chis
-# list(map(int, ch))
-# print(chis)
+data.close()
+
+
+res = 1
+i = 0
+len_chis = int(len(chis))
+print(f'Длинна массива индексов {len_chis}')
+for i in range(0, len_chis, 1):
+    print(f'индекс i  {i}')
+    io = chis[i]
+    io = int(io)
+    vr = order[io]
+    vr = int(vr)
+    print(f'число строки {vr}')
+    res *= vr
+
+print(f'Результат умножения чисел из строки по индексам из файла chisla.txt : {res}')
+
+
+
+
 
 
