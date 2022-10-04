e8d7781a163671229dcf3084afe33857f17fec61
Drei4 <antipin-lukinf@mail.ru>
2022-10-04 14:09:39 +0300

    add zadach 12 dodelat 2

--- dz3/zadach12.py
+++ dz3/zadach12.py
@@ -5,15 +5,31 @@
 
 
 ishodnii_spisok = [2, 3, 4, 5]
-proizvedenie_par = []
+proizvedenie_par = [0]
 
-def proizvedenie (ishodnii_spisok):
-    dlin_list = int(len(ishodnii_spisok)/2)
-    for i in range(1, len(ishodnii_spisok), 1):
-        i = int(i)
-        proizvedenie_par.append
-        proizvedenie_par[i] = str(ishodnii_spisok[i] * ishodnii_spisok(dlin_list - i))
+
+def proizvedenie (ishodnii_spisok, proizvedenie_par):
+    dlin_list = len(ishodnii_spisok)
+    x = 0
+    for i in range(len(ishodnii_spisok)):
+        #i = int(i)
+        #print(len(ishodnii_spisok))
+        print(ishodnii_spisok[- i - 1])
+        # print()
+        # proizvedenie_par[i] = int(ishodnii_spisok[i]) * int(ishodnii_spisok(dlin_list - i))
+        # proizvedenie_par.append(i)
+        
+        #proizvedenie_par[i] = int(ishodnii_spisok[i] * ishodnii_spisok(dlin_list - i))
    # if len(ishodnii_spisok) % 2 != 0
     print(ishodnii_spisok)
 
-proizvedenie(ishodnii_spisok)
+proizvedenie(ishodnii_spisok, proizvedenie_par)
+
+
+#  dlin_list = int(len(ishodnii_spisok)/2)
+#     for i in range(1, len(ishodnii_spisok), 1):
+#         i = int(i)
+#         proizvedenie_par.append
+#         proizvedenie_par[i] = str(ishodnii_spisok[i] * ishodnii_spisok(dlin_list - i))
+#    # if len(ishodnii_spisok) % 2 != 0
+#     print(ishodnii_spisok)
