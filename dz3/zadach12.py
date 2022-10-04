e2379ac7cdadb130a01adddd6a883a53cdb952ee
Drei4 <antipin-lukinf@mail.ru>
2022-10-04 20:58:24 +0300

    add zadach 12 complit

--- dz3/zadach12.py
+++ dz3/zadach12.py
@@ -4,32 +4,24 @@
 # - [2, 3, 5, 6] => [12, 15]
 
 
-ishodnii_spisok = [2, 3, 4, 5]
+ishodnii_spisok = [2, 3, 4, 5, 6, 7, 8]
 proizvedenie_par = [0]
 
 
-def proizvedenie (ishodnii_spisok, proizvedenie_par):
+def proizvedenie (ishodnii_spisok):
     dlin_list = len(ishodnii_spisok)
     x = 0
-    for i in range(len(ishodnii_spisok)):
-        #i = int(i)
-        #print(len(ishodnii_spisok))
-        print(ishodnii_spisok[- i - 1])
-        # print()
-        # proizvedenie_par[i] = int(ishodnii_spisok[i]) * int(ishodnii_spisok(dlin_list - i))
-        # proizvedenie_par.append(i)
-        
-        #proizvedenie_par[i] = int(ishodnii_spisok[i] * ishodnii_spisok(dlin_list - i))
-   # if len(ishodnii_spisok) % 2 != 0
-    print(ishodnii_spisok)
+    for i in range((len(ishodnii_spisok))//2):
+                
+        proizvedenie_par[i] = int(ishodnii_spisok[i]) * int(ishodnii_spisok[- i -1])
+        proizvedenie_par.append(i)
+            
+    
+    if len(ishodnii_spisok) % 2 != 0:
+        proizvedenie_par[((len(ishodnii_spisok)//2))] = int((ishodnii_spisok[(len(ishodnii_spisok)//2)])**2)
+    else:
+        proizvedenie_par.pop()
 
-proizvedenie(ishodnii_spisok, proizvedenie_par)
+    print(proizvedenie_par)
 
-
-#  dlin_list = int(len(ishodnii_spisok)/2)
-#     for i in range(1, len(ishodnii_spisok), 1):
-#         i = int(i)
-#         proizvedenie_par.append
-#         proizvedenie_par[i] = str(ishodnii_spisok[i] * ishodnii_spisok(dlin_list - i))
-#    # if len(ishodnii_spisok) % 2 != 0
-#     print(ishodnii_spisok)
+proizvedenie(ishodnii_spisok)
