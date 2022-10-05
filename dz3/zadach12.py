923e1301364df8b6552981835c126f883b4b561b
Drei4 <antipin-lukinf@mail.ru>
2022-10-05 21:34:06 +0300

    add zadach13_1

--- dz3/zadach12.py
+++ dz3/zadach12.py
@@ -25,3 +25,23 @@ def proizvedenie (ishodnii_spisok):
     print(proizvedenie_par)
 
 proizvedenie(ishodnii_spisok)
+
+
+
+
+
+# import math
+
+# def make_pairs_prodact():
+# try:
+# num_list = input("Enter numbers without spaces: ")
+# num_list = [int(i) for i in num_list]
+# pair_list = []
+# for i in range(math.ceil(len(num_list)/2)):
+# pair_prodact = num_list[i]*num_list[-1*(i+1)]
+# pair_list.append(pair_prodact)
+# print("\033[36m{}{}{}".format(num_list, " --> ", pair_list))
+# except ValueError:
+# print("\033[1m\033[31m{}".format("You must enter an integer!"))
+
+# make_pairs_prodact()
