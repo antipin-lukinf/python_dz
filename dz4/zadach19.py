bb76ebd8db31d9e89a28a9d15c9b9684ace6db36
Drei4 <antipin-lukinf@mail.ru>
2022-10-13 17:22:19 +0300

    add zadach19

--- dz4/zadach19.py
+++ dz4/zadach19.py
@@ -4,18 +4,28 @@
 # - k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
 # -  k=4 => 2*x^4 + 4*x^3 + 5x^2  - 3x + 3 = 0
 
-import numpy as np
-from random import randint
-k = randint(0, 10)
-#print (k)
+# import numpy as np
+import random
+k = int(input('Введите максимальную степень многочлена : '))
 
+def mnog(value):
+    lst = []
+    eq = ' = 0'
+    for i in range(0, value + 1):
+        lst.append(random.randint(0, 100))
+        if lst[i] != 0:
+            if i == 0:
+                eq = str(lst[i]) + eq
+            elif i == 1:
+                eq = str(lst[i]) + ' *x + '+ eq
+            else:
+                eq = str(lst[i]) + ' *x^' + str(i) + ' + ' + eq 
+    return eq
 
-a= np.array([k,k-1,k-2])
-f = np.poly1d(a)
-print(f)
+mnog(k)
 
 path = 'C:\\Users\\antip\\Desktop\\учеба\\python\\DZ\\dz4\\pol.txt'
 zap = open(path, 'w')
-zap.write(f'{f}')
+zap.write(f'{mnog(k)}')
 
 zap.close()
