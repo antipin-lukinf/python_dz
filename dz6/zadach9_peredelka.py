f730c7c7e73e810edc9cc0154519a775e7ff2856
Drei4 <antipin-lukinf@mail.ru>
2022-10-15 16:25:20 +0300

    add zadach9_peredelka

--- dz6/zadach9_peredelka.py
+++ dz6/zadach9_peredelka.py
@@ -1,44 +1,24 @@
 #4) Задайте число N и создайте список, заполненный числами из промежутка [-N, N].
 # Найдите произведение элементов на указанных позициях. Позиции хранятся в файле file.txt в одной строке одно число.
 
-
 n = int(input('Введите целое число : '))
-order = []
-
-
-for i in range(-n, n+1):
-    order.append(i)
-
+order = [i for i in range(-n, n+1)]
 print(order)
 
-
-chis = []
-path = 'C:\\Users\\antip\\Desktop\\учеба\\python\\DZ\\dz2\\chisla.txt'
+path = 'C:\\Users\\antip\\Desktop\\учеба\\python\\DZ\\dz6\\chisla.txt'
 data = open(path, 'r')
-for line in data:
-    chis.append(line)
+chis = [line for line in data]
 print(chis)
-
 data.close()
 
-
 res = 1
-i = 0
-len_chis = int(len(chis))
-print(f'Длинна массива индексов {len_chis}')
-for i in range(0, len_chis, 1):
-    print(f'индекс i  {i}')
+for i in range(len(chis)):
     io = chis[i]
     io = int(io)
     vr = order[io]
-    vr = int(vr)
-    print(f'число строки {vr}')
     res *= vr
 
 print(f'Результат умножения чисел из строки по индексам из файла chisla.txt : {res}')
-
-
-
 
 
 
